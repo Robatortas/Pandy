b6358dd680b36c381424fd061949cb5f461815af
Robatortas <robatortas1@gmail.com>
2023-09-27 10:33:44 -0700

    Buttons Work

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ document.addEventListener("scroll", (event) => {
     document.getElementById("header-image").style.objectPosition = "0vw " + scrollY/3 + "px";
     //document.getElementById("header-image").style.height = 100 + scrollY/10 + "vh";
     document.getElementById("header-image").style.filter = "brightness(" + (60+(-scrollY/100)) + "%) blur(" + (3+(scrollY/100)) + "px)";
-}); 
+});
+
+document.querySelectorAll("dockChild").forEach(v => {
+    v.addEventListener("click", (event) => {
+    });
+});
