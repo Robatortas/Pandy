0da515586cbb9976ffbcc9ce92902c0e319f2522
Robatortas <robatortas1@gmail.com>
2023-09-28 12:31:15 -0700

    Actions

--- script.js
+++ script.js
@@ -4,11 +4,5 @@ const headerImage = document.getElementById("header-image");
 
 document.addEventListener("scroll", (event) => {
     document.getElementById("header-image").style.objectPosition = "0vw " + scrollY/3 + "px";
-    //document.getElementById("header-image").style.height = 100 + scrollY/10 + "vh";
-    document.getElementById("header-image").style.filter = "brightness(" + (60+(-scrollY/100)) + "%) blur(" + (3+(scrollY/100)) + "px)";
-});
-
-document.querySelectorAll("dockChild").forEach(v => {
-    v.addEventListener("click", (event) => {
-    });
+    document.getElementById("header-image").style.filter = "brightness(" + (60+(-scrollY/100)) + "%) blur(" + ((scrollY/100)) + "px)";
 });
