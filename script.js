3dd38df53583ebf031810e2057ebf387de636e15
Robatortas <robatortas1@gmail.com>
2023-10-03 22:41:42 -0700

    Changes

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ const headerImage = document.getElementById("header-image");
 document.addEventListener("scroll", (event) => {
     document.getElementById("header-image").style.objectPosition = "0vw " + scrollY/3 + "px";
     document.getElementById("header-image").style.filter = "brightness(" + (60+(-scrollY/100)) + "%) blur(" + ((scrollY/100)) + "px)";
+    
+    document.getElementById("actions_image").style.objectPosition = "0vw " + (-document.getElementById("actions_image").offsetWidth/3 + scrollY/3) + "px";
+    document.getElementById("actions_image").style.filter = "blur(" + (-document.getElementById("actions_image").offsetWidth/3 + (scrollY/100)) + "px)";
 });
