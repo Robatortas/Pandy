62eb1bbe6bf908c9b1228254944775692b58da84
Robatortas <robatortas1@gmail.com>
2023-10-04 21:33:25 -0700

    Text container improvements

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const headerImage = document.getElementById("header-image");
 document.addEventListener("scroll", (event) => {
     document.getElementById("header-image").style.objectPosition = "0vw " + scrollY/3 + "px";
     document.getElementById("header-image").style.filter = "brightness(" + (60+(-scrollY/100)) + "%) blur(" + ((scrollY/100)) + "px)";
-    
-    document.getElementById("actions_image").style.objectPosition = "0vw " + (-document.getElementById("actions_image").offsetWidth/3 + scrollY/3) + "px";
-    document.getElementById("actions_image").style.filter = "blur(" + (-document.getElementById("actions_image").offsetWidth/3 + (scrollY/100)) + "px)";
+    //document.getElementById("actions_image").style.objectPosition = "0vw " + (-document.getElementById("actions_image").offsetWidth/6 + scrollY/5 + "px";
+    // if(document.getElementById("actions_image").style.width > scrollY)
+    // document.getElementById("actions_image").style.width = 10 + scrollY/100 + "vw";
+    //document.getElementById("actions_image").style.filter = "blur(" + ((((scrollY*3) - document.getElementById("actions_image").style.top)/2000)) + "px)";
+    if(document.getElementById("dock").getBoundingClientRect().y+document.querySelector(".header").getBoundingClientRect().height/2 <= scrollY) {
+        // document.getElementById("dock").style.objectPosition = "0px " + scrollY*10 + "px";
+        // document.getElementById("dock").getBoundingClientRect(). y = scrollY;
+        document.getElementById("dock").style.position = "fixed";
+        console.log("hey");
+    }
 });
